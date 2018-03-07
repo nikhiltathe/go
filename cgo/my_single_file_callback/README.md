1e4b7b28d596419f7ee87cffd9e8c94db4c678ba
Tathe <Nikhil.Tathe@emc.com>
2018-03-07 15:43:01 +0530

    Remove unwanted code

--- cgo/my_single_file_callback/README.md
+++ cgo/my_single_file_callback/README.md
@@ -1,3 +1,7 @@
+Usage:
+
+mkdir libs
+
 gcc -c src/clibrary.c -o libs/clibrary.o
 
 ar cru libs/libclibrary.a libs/clibrary.o
@@ -5,3 +9,7 @@ ar cru libs/libclibrary.a libs/clibrary.o
 go build
 
 ./my_single_file_callback.exe
+
+Output :
+
+4
--- cgo/my_single_file_callback/goprog.go
+++ cgo/my_single_file_callback/goprog.go
@@ -11,18 +11,9 @@ import "C"
 
 import (
 	"fmt"
-	// "unsafe"
 )
 
-//export callOnMeGo
-func callOnMeGo(in int) int {
-	fmt.Printf("Go.callOnMeGo(): called with arg = %d\n", in)
-	return in + 1
-}
-
 func main() {
-	fmt.Printf("Go.main(): calling C function with callback to us\n")
-	// C.some_c_func((C.callback_fcn)(unsafe.Pointer(C.callOnMeGo_cgo)))
 	val := C.Square(C.int(2))
 	fmt.Println(val)
 }

99edd63c279be51bcbd05e93e4a9837e7351bd68
Tathe <Nikhil.Tathe@emc.com>
2020-12-08 20:35:22 +0530

    CAll order service

--- gorillaMux/user/handler/handler.go
+++ gorillaMux/user/handler/handler.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	log "github.com/go/gorillaMux/logger"
@@ -29,13 +31,48 @@ func UsersHandler(w http.ResponseWriter, r *http.Request) {
 	log.Info("Searching for ", id)
 	name := DB.Getuser(id)
 
-	if name != "" {
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(name)
+	if name == "" {
+		log.Error("No user found")
+		json.NewEncoder(w).Encode("No ID found")
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	json.NewEncoder(w).Encode("No ID found")
-	w.WriteHeader(http.StatusNotFound)
+	w.WriteHeader(http.StatusOK)
+	// Get corresponding order too
+	order, err := getOrderDetails(id)
+	if err != nil {
+		log.Error(err)
 
+		json.NewEncoder(w).Encode(name + " No order found")
+	}
+	json.NewEncoder(w).Encode(name + " order is :" + order)
+
+}
+
+func getOrderDetails(id string) (string, error) {
+	log.Debug("Entering")
+	defer log.Debug("Exiting")
+
+	url := "http://127.0.0.1:9002/orders/" + id
+	log.Debug("URL is : ", url)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	httpClient := &http.Client{Transport: tr}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	return string(data[:]), err
 }

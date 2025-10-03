8824a381742db0ce4ea4a6fca6a34f6d665ece6a
tathen <nikhil.tathe@dell.com>
2025-10-03 06:22:28 -0400

    Add Product except self

---
+++ LeetCode/product_except_self.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	ans := productExceptSelf(nums)
+	fmt.Println(ans)
+}
+
+func productExceptSelf(nums []int) []int {
+	// 1. Create an empty answer array
+	ans := make([]int, len(nums))
+	postFix := make([]int, len(nums))
+
+	product := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		postFix[i] = product
+		product = product * nums[i]
+	}
+	fmt.Println(postFix)
+
+	product = 1
+	for i := 0; i < len(nums); i++ {
+		ans[i] = postFix[i] * product
+		product = product * nums[i]
+	}
+
+	return ans
+}
